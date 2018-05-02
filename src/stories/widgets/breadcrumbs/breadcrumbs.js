35fea6f919e7bbeb76c867214062ca85041bdb17
Michael Wonng <michael@wonng.com>
2018-05-02 17:00:27 +1000

    chagne text

--- src/stories/widgets/breadcrumbs/breadcrumbs.js
+++ src/stories/widgets/breadcrumbs/breadcrumbs.js
@@ -5,8 +5,8 @@ const Breadcrumb = () => {
   return(
     <div className="breadcrumbs">
       <ul id="breadcrumb">
-        <li className="completed"><a href="#"><span class="icon icon-beaker"> </span> Addon</a></li>
-        <li><a href="#"><span class="icon icon-double-angle-right"></span> Config</a></li>
+        <li className="completed"><a href="#"><span class="icon icon-beaker"> </span>Payment</a></li>
+        <li><a href="#"><span class="icon icon-double-angle-right"></span>Addons</a></li>
         <li><a href="#"><span class="icon icon-rocket"> </span>Review</a></li>
         <li className="current"><a href="#"><span class="icon icon-arrow-down"> </span>Apply</a></li>
       </ul>
