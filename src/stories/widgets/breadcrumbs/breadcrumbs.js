3ba1ec691113c57b3702ea6ef36fa122d07d1418
Michael Wonng <michael@wonng.com>
2018-05-03 13:21:20 +1000

    add table breadcrumb

--- src/stories/widgets/breadcrumbs/breadcrumbs.js
+++ src/stories/widgets/breadcrumbs/breadcrumbs.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import './breadcrumbs.scss';
-
+import './table_breadcrumbs.scss';
 const Breadcrumb = () => {
   return(
     <div className="breadcrumbs">
       <ul id="breadcrumb">
-        <li className="completed"><a href="#"><span class="icon icon-beaker"> </span>Payment</a></li>
-        <li><a href="#"><span class="icon icon-double-angle-right"></span>Addons</a></li>
-        <li><a href="#"><span class="icon icon-rocket"> </span>Review</a></li>
-        <li className="current"><a href="#"><span class="icon icon-arrow-down"> </span>Apply</a></li>
+        <li className="completed">
+          <a href="#">
+            Choose Contract
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Addons select
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Review payment
+          </a>
+        </li>
+        <li className="current">
+          <a href="#">
+            Apply online
+          </a>
+        </li>
       </ul>
     </div>
+    )
+}
+
+const TableBreadcrumb = () => {
+  return(
+    <table id="table-breadcrumb">
+      <tr>
+        <td className="completed">
+          <span><a href="#">Change role</a></span>
+        </td>
+        <td className="selected">
+          <span>Switch Banner</span>
+        </td>
+        <td>
+          <span>Dig hole</span>
+        </td>
+        <td>
+          <span>Whispering story</span>
+        </td>
+      </tr>
+    </table>
   )
 }
 
-export default Breadcrumb
+export default TableBreadcrumb
